18cc3062bd7bc0e27c29d0e0e94d44fb9d4defd6
Alger <AlgerDu@outlook.com>
2019-03-11 23:23:07 +0800

    good

--- D.VersionTool.Core/Projects/XmlProjectFileTmplate.cs
+++ D.VersionTool.Core/Projects/XmlProjectFileTmplate.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Xml;
 
@@ -36,7 +37,8 @@ namespace D.Tool.Version.Core
 
             if (nodes.Count != 1)
             {
-
+                var node = makeXPath(_xml, _xml, nodePath);
+                node.InnerText = value;
             }
             else
             {
@@ -44,6 +46,26 @@ namespace D.Tool.Version.Core
 
                 node.InnerText = value;
             }
+        }
+
+
+
+        private XmlNode makeXPath(XmlDocument doc, XmlNode parent, string xpath)
+        {
+            // grab the next node name in the xpath; or return parent if empty
+            string[] partsOfXPath = xpath.Trim('/').Split('/');
+            string nextNodeInXPath = partsOfXPath.First();
+            if (string.IsNullOrEmpty(nextNodeInXPath))
+                return parent;
+
+            // get or create the node from the name
+            XmlNode node = parent.SelectSingleNode(nextNodeInXPath);
+            if (node == null)
+                node = parent.AppendChild(doc.CreateElement(nextNodeInXPath));
+
+            // rejoin the remainder of the array as an xpath expression and recurse
+            string rest = String.Join("/", partsOfXPath.Skip(1).ToArray());
+            return makeXPath(doc, node, rest);
         }
     }
 }
